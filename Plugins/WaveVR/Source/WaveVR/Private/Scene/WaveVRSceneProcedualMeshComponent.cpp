b0e40a43a4d49d096b3b22ac6806117875a20093
zec37 <zhaiguanxun@hotmail.com>
2024-10-24 14:47:20 +0800

    Remove Plugins folder
    Suggest to add plugins with 6.1.0 or newer if cloned

--- Plugins/WaveVR/Source/WaveVR/Private/Scene/WaveVRSceneProcedualMeshComponent.cpp
+++
@@ -1,9 +0,0 @@
-#include "Scene/WaveVRSceneProcedualMeshComponent.h"
-
-#include "Logging/LogMacros.h"
-
-DEFINE_LOG_CATEGORY_STATIC(WVRScenePMC, Log, All);
-
-UWaveVRSceneProcedualMeshComponent::UWaveVRSceneProcedualMeshComponent(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer) {
-	bUseComplexAsSimpleCollision = false;
-}
--- Plugins/WaveVR/Source/WaveVR/Private/commit_info.h
+++
@@ -1,4 +0,0 @@
-#define WAVEVR_COMMIT_INFO "\
-master \
-commit f634d2b3f57a81e5e73b9a8d4e7cdf987a69bf1d\
-"
